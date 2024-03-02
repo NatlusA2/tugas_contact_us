74bacb9d3441bbccc1110c0cd9263e3c92519858
NatlusA2 <sultanalif332@gmail.com>
2024-03-03 03:05:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var map = L.map('map').setView([-6.309168238001754, 106.10664183431244], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
